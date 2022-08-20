19df9927a34e672b982d8d23042dcea7cbcc61da
ofgonen <ofgonen@gmail.com>
2022-08-21 01:06:57 +0300

    Implement Collectable

---
+++ Assets/Script/Collectable.cs
@@ -0,0 +1,44 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+using Sirenix.OdinInspector;
+
+public class Collectable : MonoBehaviour
+{
+#region Fields
+  [ Title( "Setup" ) ]
+    [ SerializeField ] ColorData data_color; 
+    [ SerializeField ] int data_value = 1; 
+
+  [ Title( "Shared Variable" ) ]
+    [ SerializeField ] ColorData data_color_player; 
+    [ SerializeField ] PlayerLength property_player_length; 
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+    public void OnTrigger()
+    {
+        if( data_color.ColorID == data_color_player.ColorID )
+			property_player_length.Add( data_value );
+        else
+			property_player_length.Substact( data_value );
+	}
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
