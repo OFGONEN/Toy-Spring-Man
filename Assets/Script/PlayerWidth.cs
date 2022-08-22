eb3fd65b822767eb58219cff917c6d512c25f34e
ofgonen <ofgonen@gmail.com>
2022-08-22 18:00:13 +0300

    PlayerWidth: Ratio Property

--- Assets/Script/PlayerWidth.cs
+++ Assets/Script/PlayerWidth.cs
@@ -8,6 +8,8 @@ using FFStudio;
 [ CreateAssetMenu( fileName = "property_player_width", menuName = "FF/Game/Property/Player Width" ) ]
 public class PlayerWidth : SharedFloatNotifier
 {
+	public float Ratio => sharedValue / 100f;
+
 	public void Add( float value )
 	{
 		SharedValue = Mathf.Min( 100, sharedValue + value );
