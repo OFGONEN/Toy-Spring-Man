0aa88f3b1ceb57e5d13e1979f4410d4d4092986d
ofgonen <ofgonen@gmail.com>
2022-08-22 18:04:39 +0300

    Implement Spring Value

---
+++ Assets/Script/SpringValue.cs
@@ -0,0 +1,30 @@
+/* Created by and for usage of FF Studios (2021). */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FFStudio;
+
+[ CreateAssetMenu( fileName = "shared_spring_value", menuName = "FF/Game/Property/Spring Value" ) ]
+public class SpringValue : SharedFloat
+{
+#region Fields
+#endregion
+
+#region Properties
+#endregion
+
+#region Unity API
+#endregion
+
+#region API
+#endregion
+
+#region Implementation
+#endregion
+
+#region Editor Only
+#if UNITY_EDITOR
+#endif
+#endregion
+}
