9ab023469a496edf15bc31b962e3fa016e5cca67
ofgonen <ofgonen@gmail.com>
2022-08-25 14:52:56 +0300

    🐞PlayerWidth: Correctly punch shared value

--- Assets/Script/PlayerWidth.cs
+++ Assets/Script/PlayerWidth.cs
@@ -13,6 +13,7 @@ public class PlayerWidth : SharedFloatNotifier
 	RecycledTween recycledTween = new RecycledTween();
 
 	Vector3 punch;
+	float baseValue;
 
 	public float Ratio      => sharedValue / GameSettings.Instance.spring_width_max;
 	public float BlendRatio => sharedValue * 100f / GameSettings.Instance.spring_width_max;
@@ -34,7 +35,8 @@ public class PlayerWidth : SharedFloatNotifier
 	[ Button() ]
 	void Punch()
 	{
-		punch = Vector3.up * sharedValue;
+		punch     = Vector3.zero;
+		baseValue = sharedValue;
 
 		recycledTween.Recycle( DOTween.Punch( GetPunch, SetPunch, Vector3.up,
 			GameSettings.Instance.spring_punch_lateral_duration,
@@ -46,7 +48,7 @@ public class PlayerWidth : SharedFloatNotifier
 
 	void OnPunchUpdate()
 	{
-		SharedValue = punch.y;
+		SharedValue = baseValue + baseValue * punch.y;
 	}
 
 	Vector3 GetPunch()
